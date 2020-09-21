babfe67d750a391490b415dfe361f3210a260bc5
Danilo Xavier <daniloxaviergo@gmail.com>
2020-09-20 21:57:07 -0300

    adicionado minimize to alt, validando se a janela existe e trabalhando com window class

--- goto_saved_window.py
+++ goto_saved_window.py
@@ -10,6 +10,11 @@ jjson = json.loads(str_json)
 
 key_json = sys.argv[1]
 line = jjson[key_json]
+by_class_name = line[0] != '0'
+
+if by_class_name:
+  os.popen('/home/danilo/scripts/dmenu/focus_class_name.py {class_window}'.format(class_window=line)).read()
+  sys.exit()
 
 first_part = line.split('xavier')[0]
 attrs = re.findall(r'[^\s]+', first_part)
@@ -19,13 +24,23 @@ workspace = attrs[1]
 
 current_workspace = os.popen("wmctrl -d | grep '*' | cut -d ' ' -f1").read()
 current_workspace = re.sub(r'(\r\n\t|\n|\r\t|\n)', '', current_workspace)
-change_workspace  = key_json[:3] != 'alt'
+common_behavior   = key_json[:3] != 'alt'
 
-if current_workspace != workspace and change_workspace:
+window_exists = len(os.popen("wmctrl -dliGux | grep {win_id}".format(win_id=win_id)).read()) > 0
+if not window_exists:
+  sys.exit()
+
+if current_workspace != workspace and common_behavior:
   os.popen("xdotool set_desktop {work}".format(work=workspace)).read()
 
-if not change_workspace:
+if not common_behavior:
   os.popen('wmctrl -r -R -i -a {win_id} -t {work}'.format(win_id=win_id, work=current_workspace)).read()
+
+  # if focus windows minimize
+  active_window = os.popen('printf 0x%x "$(xdotool getwindowfocus)"').read()
+  if int(active_window, 16) == int(win_id, 16):
+    os.popen('xdotool getactivewindow windowminimize').read()
+    sys.exit()
 
 os.popen('wmctrl -i -a {win_id}'.format(win_id=win_id)).read()
 sys.exit()
